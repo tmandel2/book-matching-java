32eb435a6d5423dc9c1767a99d1213a2557ec803
Tim Mandel <timothy.mandel@gmail.com>
2019-02-25 15:43:35 -0600

    User up to controller created

---
+++ src/main/java/bookmatching/UserRepository.java
@@ -0,0 +1,7 @@
+package bookmatching;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByUsername(String username);
+}
