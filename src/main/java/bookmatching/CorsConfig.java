68e82acf460dae0365ac7e610eff9a39836ef450
Tim Mandel <timothy.mandel@gmail.com>
2019-03-04 09:59:49 -0600

    Multiple cross origins

--- src/main/java/bookmatching/CorsConfig.java
+++ src/main/java/bookmatching/CorsConfig.java
@@ -8,16 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-
+    //When loading up to heroku, need to comment out the second allowedOrigins.
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//When loading up to heroku, need to comment out the second allowedOrigins.
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://book-match-react.herokuapp.com")
-                .allowedOrigins(
-                        "http://localhost:3000"
-                )
+                        "https://book-match-react.herokuapp.com","http://localhost:3000")
+//                .allowedOrigins(
+//                        "http://localhost:3000"
+//                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");
